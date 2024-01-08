fd8dc88db03c032d1c5a546f363b4d552c2beaf0
Shikhar Barthwal <76698852+yato-dev@users.noreply.github.com>
2024-01-09 00:33:09 +0530

    BankingCash

---
+++ src/DS/BankingCash/BankingCash.java
@@ -0,0 +1,48 @@
+
+package org.example.DS_Programs.BankingCash;
+import java.util.*;
+public class BankingCash {
+    static int cashBalance = 10000;
+   
+    public static void deposit(Queue queue, int amount) {
+        cashBalance += amount;
+        System.out.println("Deposited: $" + amount + " | Cash Balance: $" + cashBalance);
+
+    }
+  
+    public static void withdraw(Queue queue, int amount) {
+        if (cashBalance >= amount) {
+            cashBalance -= amount;
+            System.out.println("Withdrawn: $" + amount + " | Cash Balance: $" + cashBalance);
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(10); 
+
+      
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3); 
+
+        while (!queue.isEmpty()) {
+            int choice = queue.dequeue();
+            if (choice == 1) {
+             
+                int depositAmount = 500; 
+                deposit(queue, depositAmount);
+            } else if (choice == 2) {
+               
+                int withdrawAmount = 300; 
+                withdraw(queue, withdrawAmount);
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+
+        
+    }
+}
---
+++ src/DS/BankingCash/Queue.java
@@ -0,0 +1,48 @@
+package org.example.DS_Programs.BankingCash;
+class Queue {
+    private int front, rear, size;
+    private int capacity;
+    private int[] array;
+
+
+    Queue(int capacity) {
+        this.capacity = capacity;
+        front = this.size = 0;
+        rear = capacity - 1;
+        array = new int[this.capacity];
+    }
+    
+    boolean isFull() {
+        return (this.size == this.capacity);
+    }
+    
+    boolean isEmpty() {
+        return (this.size == 0);
+    }
+
+    void enqueue(int item) {
+        if (isFull()) {  
+            System.out.println("Queue is full, cannot add more people.");
+            return;
+        }
+        this.rear = (this.rear + 1) % this.capacity;
+        this.array[this.rear] = item;
+        this.size = this.size + 1;
+    }
+
+    int dequeue() {
+        if (isEmpty()) {  
+            System.out.println("Queue is empty, no people to dequeue.");
+            return Integer.MIN_VALUE;
+        }
+        int item = this.array[this.front];
+        this.front = (this.front + 1) % this.capacity;
+        this.size = this.size - 1;
+        return item;
+    }
+
+    public int getFront() {
+        return front;
+    }
+}
+
